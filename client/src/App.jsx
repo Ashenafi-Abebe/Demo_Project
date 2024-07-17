13f239c0c77382760896914828a164d28cd414ac
Ashemarc <152915640+Ashenafi-Abebe@users.noreply.github.com>
2024-07-16 20:58:51 -0700

    feat: Add initial application structure with Loading and Hero components
    - Implemented the main App.jsx with loading state management
    - Created Loading component to display loading indicator
    - Created Hero component to display main content after loading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,8 @@
-import React from "react";
-import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Router.jsx";
 
 function App() {
-  return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
